779a261b80ea28364371f1413226a51b7b619a5f
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-09-07 16:59:40 +0800

    feat: add swagger doc

--- application/http.go
+++ application/http.go
@@ -1,13 +1,23 @@
 package application
 
 import (
+	docs "asiayo/docs"
 	"asiayo/exchange_rate"
 
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetupRoute() *gin.Engine {
 	router := gin.Default()
+
+	docs.SwaggerInfo.Title = "AsiaYo exchange rate API"
+	docs.SwaggerInfo.Description = ""
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:8080"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	v1 := router.Group("/api/v1")
 	{
