6c89ab9caf98b34c45e3e484e9d19e122836309f
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-11-02 21:45:17 +0800

    refactor (exchange rate): restful payload

--- exchange_rate/handler.go
+++ exchange_rate/handler.go
@@ -13,28 +13,52 @@ import (
 	"golang.org/x/text/message"
 )
 
+type ExchangeRateHandler struct {
+}
+
+func NewExchangeRateHandler() ExchangeRateHandler {
+	return ExchangeRateHandler{}
+}
+
 // ExchangeRateConversion godoc
 // @Summary exchange rate conversion
 // @Description exchange rate conversion
 // @Tags exchange rate
 // @Produce json
-// @Param source query string true "source currency" Enums(JPY, TWD, USD)
-// @Param target query string true "target currency" Enums(JPY, TWD, USD)
+// @Param source path string true "source currency" Enums(JPY, TWD, USD)
+// @Param target path string true "target currency" Enums(JPY, TWD, USD)
 // @Param amount query string true "amount" example("$1,111.05")
 // @Success 200 {object} exchange_rate.ConversionResponse "Success"
 // @Failure 400 {object} exchange_rate.ConversionResponse "Bad parameter"
-// @Router /exchange-rate [get]
-func Conversion(ctx *gin.Context) {
+// @Router /v1/exchange-rate/{source}/conversion/{target} [get]
+func (h *ExchangeRateHandler) Conversion(ctx *gin.Context) {
+	var param ConversionParam
 	var query ConversionQuery
-	if err := ctx.BindQuery(&query); err != nil {
-		ctx.JSON(http.StatusBadRequest, NewErrorResponse(fmt.Sprintf("Bad parameter {%v}", err.Error())))
+
+	if err := ctx.ShouldBindUri(&param); err != nil {
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse(fmt.Sprintf("Bad parameter {%v}", err.Error())))
+		return
+	}
+	source, ok := ExchangeRateFromValue(param.Source)
+	if !ok {
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse("Given currency is not supported"))
+		return
+	}
+	target, ok := ExchangeRateFromValue(param.Target)
+	if !ok {
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse("Given currency is not supported"))
+		return
+	}
+
+	if err := ctx.ShouldBindQuery(&query); err != nil {
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse(fmt.Sprintf("Bad parameter {%v}", err.Error())))
 		return
 	}
 
 	// find exchange rate from table
-	exchangeRate, ok := RateTable[query.Source][query.Target]
+	exchangeRate, ok := RateTable[source][target]
 	if !ok {
-		ctx.JSON(http.StatusBadRequest, NewErrorResponse("Given currency is not supported"))
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse("Given currency is not supported"))
 		return
 	}
 
@@ -42,7 +66,7 @@ func Conversion(ctx *gin.Context) {
 	r, _ := regexp.Compile(`^\$\d{1,3}(,\d{3})*(\.\d{2})?$`)
 	match := r.FindString(query.Amount)
 	if match == "" {
-		ctx.JSON(http.StatusBadRequest, NewErrorResponse("Given amount is in invalid format"))
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse("Given amount is not valid format"))
 		return
 	}
 	match = strings.Replace(match, ",", "", -1)
@@ -50,7 +74,7 @@ func Conversion(ctx *gin.Context) {
 
 	amount, err := strconv.ParseFloat(match, 32)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, NewErrorResponse("Given amount {%f} is not a number"))
+		ctx.JSON(http.StatusBadRequest, NewConversionErrorResponse("Given amount {%f} is not a number"))
 		return
 	}
 
--- exchange_rate/payload.go
+++ exchange_rate/payload.go
@@ -1,8 +1,11 @@
 package exchange_rate
 
+type ConversionParam struct {
+	Source string `uri:"source" binding:"required"`
+	Target string `uri:"target" binding:"required"`
+}
+
 type ConversionQuery struct {
-	Source string `form:"source" binding:"required"`
-	Target string `form:"target" binding:"required"`
 	// @Description The accepted format for the amount is a string that starts with a dollar sign,
 	// @Description followed by a positive number with up to two decimal places and thousand separators (commas).
 	Amount string `form:"amount" binding:"required"`
